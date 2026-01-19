57459f958cbfa2adb3a1efb6711033ff58224780
hae-wong <89lhwon@gmail.com>
2026-01-19 16:58:41 +0900

    second commit

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,19 +1,22 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import styles from "./Movie.module.css";
 
-function Movie({ id, coverImg, title, titleLong, genres }) {
-  return (<div>
-    <img src={coverImg} alt={title} />
-    <h2>
-      <Link to={`/movie/${id}`}>{title}</Link>
-    </h2>
-    <p>{titleLong}</p>
-    <ul>
-      {genres.map(genre => (
-        <li key={genre}>{genre}</li>
-      ))}
-    </ul>
-  </div>
+function Movie({ id, coverImg, title, year, titleLong, genres }) {
+  return (
+    <div className={styles.movie}>
+      <img className={styles.movie__img} src={coverImg} alt={title} />
+      <h2 className={styles.movie_title}>
+        <Link to={`/movie/${id}`}>{title}</Link>
+      </h2>
+      <h3 className={styles.movie__year}>{year}</h3>
+      <p>{titleLong}</p>
+      <ul className={styles.movie__genres}>
+        {genres.map(genre => (
+          <li key={genre}>{genre}</li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
